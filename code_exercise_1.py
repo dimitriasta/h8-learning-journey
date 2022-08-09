018537fe188cb7af4ddb34dbf25147e8f965697c
dimitriasta <astazeskadimitri@gmail.com>
2022-08-09 21:51:40 +0700

    just little exercise

---
+++ code_exercise_1.py
@@ -0,0 +1,13 @@
+character_name = "Bob"
+character_age = "62"
+
+print("There once was a man named " + character_name +",")
+print("he was " + character_age + " years old.")
+
+character_name = "Mike"
+character_age = "45"
+
+print("He really like the name " + character_name +",")
+print("but didn't like being " + character_age +".")
+
+print("Giraffe\nAcademy")
--- testpush.py
+++ testpush.py
@@ -1,8 +1,8 @@
-bill = print(input("how much the bill amount?"))
-tip = print(input("how much do you want to tip?"))
-person = print(input("how many person in the table?"))
+bill=int(input("how much the bill amount?"))
+tip=int(input("how much do you want to tip?"))
+person=int(input("how many person in the table?"))
 
-tot_amount = bill * (1+tip/100)
-tot_person = tot_amount / person
+tot_amount=bill*(1+tip/100)
+tot_person=tot_amount/person
 
-print(f"So each person total is tot_person")
+print("So each person total is {:.2f} dollars".format(tot_person))
